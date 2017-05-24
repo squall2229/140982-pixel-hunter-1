122a13c0708e7641149fcfe93bb56df5fe30fe0e
Vladislav <cva.smichrissoft@gmail.com>
2017-05-24 18:09:48 +0300

    Написал функцию переключения экранов

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const templates = document.querySelectorAll(`
+  #main,
+  #greeting,
+  #rules,
+  #game-1,
+  #game-2,
+  #game-3,
+  #stats`
+);
+
+const mainContainer = document.querySelector(`.central`);
+let count = 0;
+
+const changeTemplate = (i) => {
+  mainContainer.innerHTML = templates[i].cloneNode(true).innerHTML;
+};
+
+document.addEventListener(`keydown`, (evt) => {
+  if (evt.altKey && evt.keyCode === 39 && count < templates.length - 1) {
+    count += 1;
+    changeTemplate(count);
+  } else if (evt.altKey && evt.keyCode === 37 && count > 0) {
+    count -= 1;
+    changeTemplate(count);
+  }
+});
