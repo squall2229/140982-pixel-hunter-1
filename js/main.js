5509b756a33d3a64c2dee58698dddb7b5a40fdde
Vladislav <cva.smichrissoft@gmail.com>
2017-05-25 15:51:34 +0300

    отделил логику переключения экранов от слушателя событий

--- js/main.js
+++ js/main.js
@@ -12,15 +12,27 @@ const mainContainer = document.querySelector(`.central`);
 let count = 0;
 
 const changeTemplate = (i) => {
-  mainContainer.innerHTML = templates[i].cloneNode(true).innerHTML;
+  mainContainer.innerHTML = templates[i].innerHTML;
+};
+
+const nextTemplate = () => {
+  if (count < templates.length - 1) {
+    count += 1;
+    changeTemplate(count);
+  }
+};
+
+const prevTemplate = () => {
+  if (count > 0) {
+    count -= 1;
+    changeTemplate(count);
+  }
 };
 
 document.addEventListener(`keydown`, (evt) => {
-  if (evt.altKey && evt.keyCode === 39 && count < templates.length - 1) {
-    count += 1;
-    changeTemplate(count);
-  } else if (evt.altKey && evt.keyCode === 37 && count > 0) {
-    count -= 1;
-    changeTemplate(count);
+  if (evt.altKey && evt.keyCode === 39) {
+    nextTemplate();
+  } else if (evt.altKey && evt.keyCode === 37) {
+    prevTemplate();
   }
 });
