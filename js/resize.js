7da617b35eda7f683e39815105ba794596dee38d
Vladislav Chechulin <squall2229@gmail.com>
2017-06-06 01:00:32 +0300

    Добавил событие загрузки в модуль изображений

--- js/resize.js
+++ js/resize.js
@@ -4,16 +4,18 @@ const resize = (element, w, h, ...src) => {
   Array.from(images).forEach((img, i) => {
     let newImg = new Image();
     newImg.src = src[i];
-    let width = newImg.width;
-    let height = newImg.height;
+    newImg.onload = () => {
+      let width = newImg.width;
+      let height = newImg.height;
 
-    if (width >= height) {
-      img.width = w;
-      img.height = w / (width / height);
-    } else {
-      img.height = h;
-      img.width = w * (width / height);
-    }
+      if (width >= height) {
+        img.width = w;
+        img.height = w / (width / height);
+      } else {
+        img.height = h;
+        img.width = w * (width / height);
+      }
+    };
   });
 };
 
