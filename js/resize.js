b6c88a3ee537e45565dc7cdb208ddd3942c64b8e
Vladislav Chechulin <squall2229@gmail.com>
2017-06-06 00:51:37 +0300

    убрал console.log

--- js/resize.js
+++ js/resize.js
@@ -1,8 +1,6 @@
 const resize = (element, w, h, ...src) => {
   const images = element.querySelectorAll(`.game__option img`);
 
-  // console.log(element, w, h, src);
-
   Array.from(images).forEach((img, i) => {
     let newImg = new Image();
     newImg.src = src[i];
@@ -14,7 +12,7 @@ const resize = (element, w, h, ...src) => {
       img.height = w / (width / height);
     } else {
       img.height = h;
-      img.width = 458;
+      img.width = w * (width / height);
     }
   });
 };
